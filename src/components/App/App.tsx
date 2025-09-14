1e3c392fad9721833161a7e5ca58078f68c8ae53
Max <mrakul84@gmail.com>
2025-09-14 18:37:34 +0300

    axios error

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,38 +1,27 @@
 import SearchBar from "../SearchBar/SearchBar";
 import { fetchMovieServices } from "../../services/movieService";
 import { useState } from "react";
-import toast, { Toaster } from "react-hot-toast";
 import type { Movie } from "../../types/movie";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import MovieModal from "../MovieModal/MovieModal";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import Loader from "../Loader/Loader";
+import { useQuery } from "@tanstack/react-query";
 
 export default function App() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+  const [query, setQuery] = useState("");
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
-  const handleSearch = async (query: string) => {
-    try {
-      setIsError(false);
-      setIsLoading(true);
-      setMovies([]);
-      const res = await fetchMovieServices(query);
-      if (res.length === 0) {
-        toast.error("No movies found for your query");
-        return;
-      }
-
-      setMovies(res);
-    } catch {
-      setIsError(true);
-    } finally {
-      setIsLoading(false);
-    }
+  const handleSearch = (query: string) => {
+    setQuery(query);
   };
+
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["movie", query],
+    queryFn: () => fetchMovieServices(query),
+    enabled: query !== "",
+  });
 
   const onSelect = (selectMovies: Movie) => {
     setSelectedMovie(selectMovies);
@@ -49,11 +38,10 @@ export default function App() {
       <SearchBar onSubmit={handleSearch} />
       {isLoading && <Loader />}
       {isError && <ErrorMessage />}
-      <MovieGrid movies={movies} onSelect={onSelect} />
+      {data && <MovieGrid movies={data} onSelect={onSelect} />}
       {modalIsOpen && selectedMovie && (
         <MovieModal movie={selectedMovie} onClose={onClose} />
       )}
-      <Toaster position="top-right" reverseOrder={false} />
     </>
   );
 }
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -8,15 +8,19 @@ interface FetchMovieResponseProps {
   total_results: number;
 }
 
-export const fetchMovieServices = async (topic: string): Promise<Movie[]> => {
+export const fetchMovieServices = async (query: string): Promise<Movie[]> => {
   const myKey = import.meta.env.VITE_API_KEY;
+
   const options = {
+    method: "GET",
     headers: {
-      Authorization: `Bearer ${myKey} `,
+      accept: "application/json",
+      Authorization: `Bearer ${myKey}`,
     },
   };
+
   const res = await axios.get<FetchMovieResponseProps>(
-    `https://api.themoviedb.org/3/search/movie?query=${topic}`,
+    `https://api.themoviedb.org/3/search/movie?query=${query}`,
     options
   );
   return res.data.results;
