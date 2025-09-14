8cd418f09aa64774e86b92b25d0a7cdd7c1850a9
Max <mrakul84@gmail.com>
2025-09-14 19:19:00 +0300

    d

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,22 +6,28 @@ import MovieGrid from "../MovieGrid/MovieGrid";
 import MovieModal from "../MovieModal/MovieModal";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import Loader from "../Loader/Loader";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import ReactPaginate from "react-paginate";
+import css from "./App.module.css";
 
 export default function App() {
   const [query, setQuery] = useState("");
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+  const [page, setPage] = useState(2);
 
   const handleSearch = (query: string) => {
     setQuery(query);
   };
 
   const { data, isError, isLoading } = useQuery({
-    queryKey: ["movie", query],
-    queryFn: () => fetchMovieServices(query),
+    queryKey: ["movie", query, page],
+    queryFn: () => fetchMovieServices(query, page),
     enabled: query !== "",
+    placeholderData: keepPreviousData,
   });
+
+  console.log(data);
 
   const onSelect = (selectMovies: Movie) => {
     setSelectedMovie(selectMovies);
@@ -36,9 +42,22 @@ export default function App() {
   return (
     <>
       <SearchBar onSubmit={handleSearch} />
+
+      <ReactPaginate
+        pageCount={data?.total_pages ?? 0}
+        pageRangeDisplayed={5}
+        marginPagesDisplayed={1}
+        onPageChange={({ selected }) => setPage(selected + 1)}
+        forcePage={page - 1}
+        containerClassName={css.pagination}
+        activeClassName={css.active}
+        nextLabel="→"
+        previousLabel="←"
+      />
+
       {isLoading && <Loader />}
       {isError && <ErrorMessage />}
-      {data && <MovieGrid movies={data} onSelect={onSelect} />}
+      {data && <MovieGrid movies={data.results} onSelect={onSelect} />}
       {modalIsOpen && selectedMovie && (
         <MovieModal movie={selectedMovie} onClose={onClose} />
       )}
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -8,14 +8,15 @@ interface FetchMovieResponseProps {
   total_results: number;
 }
 
-export const fetchMovieServices = async (query: string): Promise<Movie[]> => {
+export const fetchMovieServices = async (query: string, page: number) => {
   const myKey = import.meta.env.VITE_API_KEY;
 
   const options = {
-    method: "GET",
     headers: {
-      accept: "application/json",
       Authorization: `Bearer ${myKey}`,
+    },
+    params: {
+      page: page,
     },
   };
 
@@ -23,5 +24,5 @@ export const fetchMovieServices = async (query: string): Promise<Movie[]> => {
     `https://api.themoviedb.org/3/search/movie?query=${query}`,
     options
   );
-  return res.data.results;
+  return res.data;
 };
